d0e76a58e56ad8f528ecbfe17b3ea99ffe11bc55
darh0ky <jgamerino@gamil.com>
2020-05-02 03:37:25 -0400

    d

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/progressivebudget", {
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/progressivebudget", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
