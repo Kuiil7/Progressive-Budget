df573af7d188a106ff66b0f717054d554aa4a364
darh0ky <jgamerino@gamil.com>
2020-05-02 08:01:13 -0400

    d

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8088;
 
 const app = express();
 
@@ -15,11 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
 
 // routes here
 app.use(require("./routes/api.js"));
