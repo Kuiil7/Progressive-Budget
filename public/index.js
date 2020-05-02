3a462c7ba18fd2c22a921b01048c6e3b841023b8
darh0ky <jgamerino@gamil.com>
2020-05-02 07:41:57 -0400

    d

--- public/index.js
+++ public/index.js
@@ -1,127 +1,155 @@
-const total = document.querySelector("#total");
-const submit = document.querySelector("#submit");
-const name = document.querySelector("#name");
-const amount = document.querySelector("#amount");
-const button = document.querySelectorAll(".button");
-const tbody = document.querySelector("#tbody");
 let transactions = [];
 let myChart;
-let totalCurrentAmount = 0;
 
-window.addEventListener("load", function () {
-    fetch("/transactions", {
-        method: "GET"
-    })
-        .then(data => data.json())
-        .then(response => {
-            transactions = response;
+fetch("/api/transaction")
+  .then(response => response.json())
+  .then(data => {
+    // save db data on global variable
+    transactions = data;
+    populateTotal();
+    populateTable();
+    populateChart();
+  });
 
-            transactions.forEach(element => {
-                totalCurrentAmount += parseInt(element.value)
-                const tr = document.createElement("tr");
-                tr.innerHTML = `
-                  <td>${element.name}</td>
-                  <td>${element.value}</td>
-                `;
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  const total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
 
-                tbody.appendChild(tr);
-            })
-            total.innerHTML = totalCurrentAmount;
-            populateChart();
-        })
-})
+  const totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
+
+function populateTable() {
+  const tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+
+  transactions.forEach(transaction => {
+    // create and populate a table row
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
 
 function populateChart() {
-    // copy array and reverse it
-    const reversed = transactions.slice().reverse();
-    let sum = 0;
+  // copy array and reverse it
+  const reversed = transactions.slice().reverse();
+  let sum = 0;
 
-    // create date labels for chart
-    const labels = reversed.map(t => {
-        const date = new Date(t.date);
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    });
+  // create date labels for chart
+  const labels = reversed.map(t => {
+    const date = new Date(t.date);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  });
 
-    // create incremental values for chart
-    const data = reversed.map(t => {
-        sum += parseInt(t.value);
-        return sum;
-    });
+  // create incremental values for chart
+  const data = reversed.map(t => {
+    sum += parseInt(t.value);
+    return sum;
+  });
 
-    // remove old chart if it exists
-    if (myChart) {
-        myChart.destroy();
-    }
+  // remove old chart if it exists
+  if (myChart) {
+    myChart.destroy();
+  }
 
-    const ctx = document.getElementById("my-chart").getContext("2d");
+  const ctx = document.getElementById("my-chart").getContext("2d");
 
-    myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-            labels,
-            datasets: [
-                {
-                    label: "Total Over Time",
-                    fill: true,
-                    backgroundColor: "#6666ff",
-                    data
-                }
-            ]
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Total Over Time",
+          fill: true,
+          backgroundColor: "#6666ff",
+          data
         }
+      ]
+    }
+  });
+}
+
+function sendTransaction(isAdding) {
+  const nameEl = document.querySelector("#t-name");
+  const amountEl = document.querySelector("#t-amount");
+  const errorEl = document.querySelector("form .error");
+
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
+    return;
+  } else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  const transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString()
+  };
+
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
+
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
+
+  // re-run logic to populate ui with new record
+  populateChart();
+  populateTable();
+  populateTotal();
+
+  // also send to server
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      } else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
     });
 }
 
-function fetchInsert(name, amount) {
-    const data = {
-        name: name,
-        value: amount,
-        date: new Date().toLocaleString("en-US")
-    }
-    transactions.unshift(data)
-    fetch("/insertTransaction", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            Accept: "application/json, text/plain, */*",
-            "Content-Type": "application/json"
-        }
-    }).then(response => {
+document.querySelector("#add-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  sendTransaction(true);
+});
 
-    }).catch(err => {
-        saveRecord(data)
-    })
-}
-button.forEach(element => {
-    element.addEventListener("click", function () {
-        const id = this.getAttribute("data-id");
-        const currentTotal = parseInt(total.innerHTML);
-        const currentName = name.value;
-        let currentAmount = parseInt(amount.value);
-        if (currentAmount === "" || currentName === "") {
-            alert("Please fill out all the fields");
-            return
-        }
-        if (id === "add") {
-            fetchInsert(currentName, currentAmount);
-            total.innerHTML = currentTotal + currentAmount;
-        } else {
-            fetchInsert(currentName, (currentAmount * -1));
-            currentAmount = currentAmount * -1;
-            total.innerHTML = currentTotal - currentAmount;
-        }
+document.querySelector("#sub-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  sendTransaction(false);
+});
 
-        const tr = document.createElement("tr");
-        const td = document.createElement("td");
-        const td2 = document.createElement("td");
-        td.append(currentName);
-        td2.append(currentAmount);
-        tr.append(td);
-        tr.append(td2);
-        tbody.prepend(tr);
-
-        name.value = "";
-        amount.value = "";
-
-        populateChart();
-    })
-})
+document.querySelector("#del-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  deletePending();
+});
--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
+const compression = require("compression");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
-// routes
-app.use(require("./routes/app.js"));
+
+// routes here
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
