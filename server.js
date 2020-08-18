1563f22fe52d7f45100900df91c0b5a4e6417753
darh0ky <jgamerino@gamil.com>
2020-08-18 02:02:14 -0700

    updating description

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useUnifiedTopology: true, 
   useFindAndModify: false
 });
 
